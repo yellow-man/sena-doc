733f47b64585eb1afb77f2e0b1dae98851818289
yellow-man <yellow-man@users.noreply.github.com>
2017-02-03 12:34:15 +0900

    ドキュメント整理 · yellow-man/sena-batch@c9ecb8b https://github.com/yellow-man/sena-batch/commit/c9ecb8b1f594e5701b0bf8f8416592ef351ceed2 ・sena-batch develop ブランチ時点をバックアップ

--- sql/ddl/1.0.0_1.1.0/00_init_ddl.sql
+++ sql/ddl/1.0.0_1.1.0/00_init_ddl.sql
@@ -1,10 +1,7 @@
 -- Creation a database. 
-CREATE DATABASE `sena_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE DATABASE `sena_secure_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- Creation an user.for the database. 
-GRANT ALL PRIVILEGES ON `sena_db`.* TO username@localhost IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `sena_db`.* TO username@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON `sena_secure_db`.* TO username@localhost IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON `sena_secure_db`.* TO username@'%' IDENTIFIED BY 'password';
 
